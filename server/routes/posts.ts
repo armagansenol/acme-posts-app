5856473c947b55e957d7fc69700cd3f20bdce8a4
armagansenol <armagansnl@gmail.com>
2024-10-22 19:41:01 +0300

    add posts route

---
+++ server/routes/posts.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from "express"
+import axios from "axios"
+
+const router = Router()
+
+export const all = router.get("/posts", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data" })
+  }
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,14 @@
-import express from "express"
 import cors from "cors"
+import express from "express"
+
+import postsRouter from "./routes/posts"
 
 const app = express()
 const PORT = 4001
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
   })
 )
@@ -14,6 +16,9 @@ app.use(
 app.get("/", (req, res) => {
   res.send("All systems normal.")
 })
+
+// posts
+app.use("/api", postsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
